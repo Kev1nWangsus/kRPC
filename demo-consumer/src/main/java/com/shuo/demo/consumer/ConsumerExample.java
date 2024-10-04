cbff85d0d0375eac02d1b7cdeeaf0df4421c0085
Shuo Wang <1875689740ws@gmail.com>
2024-10-03 23:25:05 -0700

    chore: added a demo for loading configurations

--- demo-consumer/src/main/java/com/shuo/demo/consumer/ConsumerExample.java
+++ demo-consumer/src/main/java/com/shuo/demo/consumer/ConsumerExample.java
@@ -1,7 +1,17 @@
 package com.shuo.demo.consumer;
 
+import com.shuo.krpc.config.RpcConfig;
+import com.shuo.krpc.utils.ConfigUtils;
+
 /**
  * @author <a href="https://github.com/Kev1nWangsus">shuo</a>
  */
 public class ConsumerExample {
+    public static void main(String[] args) {
+        RpcConfig rpcConfig = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        System.out.println(rpcConfig);
+    }
+
+
+
 }
--- demo-provider/src/main/java/com/shuo/provider/ProviderExample.java
+++ demo-provider/src/main/java/com/shuo/provider/ProviderExample.java
@@ -1,7 +1,24 @@
 package com.shuo.provider;
 
+import com.shuo.demo.common.service.UserService;
+import com.shuo.krpc.RpcApplication;
+import com.shuo.krpc.registry.LocalRegistry;
+import com.shuo.krpc.server.HttpServer;
+import com.shuo.krpc.server.VertxHttpServer;
+
 /**
  * @author <a href="https://github.com/Kev1nWangsus">shuo</a>
  */
 public class ProviderExample {
+    public static void main(String[] args) {
+        // RPC framework initialization
+        RpcApplication.init();
+
+        // Register services
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
+        // Start the web server
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+    }
 }
