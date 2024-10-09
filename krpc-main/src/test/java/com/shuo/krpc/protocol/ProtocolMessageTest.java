8aff804cfc60d64938325329abf83e22a1e884c3
Shuo Wang <1875689740ws@gmail.com>
2024-10-09 01:00:41 -0700

    test: added unit test for protocol message encoder and decoder

---
+++ krpc-main/src/test/java/com/shuo/krpc/protocol/ProtocolMessageTest.java
@@ -0,0 +1,79 @@
+package com.shuo.krpc.protocol;
+
+import cn.hutool.core.util.IdUtil;
+import com.shuo.krpc.constant.RpcConstant;
+import com.shuo.krpc.model.RpcRequest;
+import io.vertx.core.buffer.Buffer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ProtocolMessageTest {
+
+    @Test
+    public void testEncodeAndDecode() throws IOException {
+        // Construct message
+        ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+        ProtocolMessage.Header header = new ProtocolMessage.Header();
+        header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+        header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+        header.setSerializer((byte) ProtocolMessageSerializerEnum.JDK.getKey());
+        header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+        header.setStatus((byte) ProtocolMessageStatusEnum.OK.getValue());
+        header.setRequestId(IdUtil.getSnowflakeNextId());
+        header.setBodyLength(0);
+        RpcRequest rpcRequest = new RpcRequest();
+        rpcRequest.setServiceName("myService");
+        rpcRequest.setMethodName("myMethod");
+        rpcRequest.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
+        rpcRequest.setParameterTypes(new Class[]{String.class});
+        rpcRequest.setArgs(new Object[]{"aaa", "bbb"});
+        protocolMessage.setHeader(header);
+        protocolMessage.setBody(rpcRequest);
+
+        Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+        ProtocolMessage<?> message = ProtocolMessageDecoder.decode(encodeBuffer);
+        Assert.assertNotNull(message);
+        Assert.assertEquals(protocolMessage.getHeader().getMagic(), message.getHeader().getMagic());
+        Assert.assertEquals(protocolMessage.getHeader().getVersion(), message.getHeader().getVersion());
+        Assert.assertEquals(protocolMessage.getHeader().getSerializer(), message.getHeader().getSerializer());
+        Assert.assertEquals(protocolMessage.getHeader().getType(), message.getHeader().getType());
+        Assert.assertEquals(protocolMessage.getHeader().getStatus(), message.getHeader().getStatus());
+        Assert.assertEquals(protocolMessage.getHeader().getRequestId(), message.getHeader().getRequestId());
+        Assert.assertEquals(protocolMessage.getBody(), message.getBody());
+    }
+
+    @Test
+    public void testEncodeAndDecodeWithJsonSerializer() throws IOException {
+        // Construct message with JSON serializer
+        ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+        ProtocolMessage.Header header = new ProtocolMessage.Header();
+        header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+        header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+        header.setSerializer((byte) ProtocolMessageSerializerEnum.JSON.getKey());
+        header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+        header.setStatus((byte) ProtocolMessageStatusEnum.OK.getValue());
+        header.setRequestId(IdUtil.getSnowflakeNextId());
+        header.setBodyLength(0);
+        RpcRequest rpcRequest = new RpcRequest();
+        rpcRequest.setServiceName("myService");
+        rpcRequest.setMethodName("myMethod");
+        rpcRequest.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
+        rpcRequest.setParameterTypes(new Class[]{String.class});
+        rpcRequest.setArgs(new Object[]{"aaa", "bbb"});
+        protocolMessage.setHeader(header);
+        protocolMessage.setBody(rpcRequest);
+
+        Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+        ProtocolMessage<?> message = ProtocolMessageDecoder.decode(encodeBuffer);
+        Assert.assertNotNull(message);
+        Assert.assertEquals(protocolMessage.getHeader().getMagic(), message.getHeader().getMagic());
+        Assert.assertEquals(protocolMessage.getHeader().getVersion(), message.getHeader().getVersion());
+        Assert.assertEquals(protocolMessage.getHeader().getSerializer(), message.getHeader().getSerializer());
+        Assert.assertEquals(protocolMessage.getHeader().getType(), message.getHeader().getType());
+        Assert.assertEquals(protocolMessage.getHeader().getStatus(), message.getHeader().getStatus());
+        Assert.assertEquals(protocolMessage.getHeader().getRequestId(), message.getHeader().getRequestId());
+        Assert.assertEquals(protocolMessage.getBody(), message.getBody());
+    }
+}
