b6aa93273e2484b23b2d7b01ebbc44072ba2ab01
Shuo Wang <1875689740ws@gmail.com>
2024-10-10 20:56:59 -0700

    feat: added RetryStrategyFactory to obtain retry strategies

---
+++ krpc-main/src/main/java/com/shuo/krpc/fault/retry/RetryStrategyKeys.java
@@ -0,0 +1,13 @@
+package com.shuo.krpc.fault.retry;
+
+/**
+ * Retry Strategy Key Constants
+
+ * @author <a href="https://github.com/Kev1nWangsus">shuo</a>
+ */
+public interface RetryStrategyKeys {
+
+    String NO = "no";
+    String FIXED_INTERVAL = "fixedInterval";
+
+}
